cf585d3623bfcd08adfa44a3be54e261d0f13994
Shouta Kamogawa <kamoshou911@icloud.com>
2019-07-19 21:24:54 +0900

    Compelete_alpha

---
+++ Shinobu_Mono/Assets/Scripts/GameStart.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//追加
+public class GameStart : MonoBehaviour {
+	public void OnStartButtonClicked() {
+		SceneManager.LoadScene ("Main");
+	}
+}
