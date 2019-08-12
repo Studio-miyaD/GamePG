aebf9509dbe6b725712a35caa9b476ff8cc296f1
Shouta Kamogawa <kamoshou911@icloud.com>
2019-08-12 14:51:57 +0900

    bug fix compile

--- Shinobu_Mono/Assets/Import Assetes/TileBuilderPackage/Editor/Utilities/AutoSaveOnPlayMode.cs
+++ Shinobu_Mono/Assets/Import Assetes/TileBuilderPackage/Editor/Utilities/AutoSaveOnPlayMode.cs
@@ -19,7 +19,7 @@ public class AutoSaveOnPlayMode
 								Debug.Log ("Auto-Saving scene before entering Play mode: " + EditorApplication.currentScene);
                 
 								EditorApplication.SaveScene ();
-								EditorApplication.SaveAssets ();
+								AssetDatabase.SaveAssets ();
 						}  
 				};   
 		}
--- Shinobu_Mono/Assets/Scripts/Unitychan.cs
+++ Shinobu_Mono/Assets/Scripts/Unitychan.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player : MonoBehaviour 
+public class Unitychan : MonoBehaviour 
 	{
 
 	public float speed = 4f; //歩くスピード
