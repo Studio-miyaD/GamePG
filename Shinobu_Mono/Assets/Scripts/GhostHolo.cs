fc9cd8bc2069c2017f2f9cfaebe0b958f5eae531
Shouta Kamogawa <kamoshou911@icloud.com>
2019-08-22 16:12:05 +0900

    .01: Auto stash before merge of "sinobu_beta.01" and "origin/sinobu_beta.01"

--- Shinobu_Mono/Assets/Scripts/GhostHolo.cs
+++ Shinobu_Mono/Assets/Scripts/GhostHolo.cs
@@ -2,20 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GhostHolo : MonoBehaviour {
+public class GhostHolo : MonoBehaviour
+{
+	Rigidbody2D rigidbody2D;
+	public int speed = 2;
+	//爆発処理1
+	public GameObject explosion;
+	//HP
+	public int attackPoint = 10;
+	public GameObject item;
+	private Life lifeScript;
+	//待機
+	private const string MAIN_CAMERA_TAG_NAME = "MainCamera";
+	//カメラに写っているかの判定
+	private bool _isRendered = false;
+	//Ghost
+	public int ghost_m = 1;
+	float TimeCount = 4;
 
-	GameObject Player;
-	GameObject homingObj;
-	public float Speed = 1f;
-
-	// Use this for initialization
-	void Start () {
-		Player = GameObject.Find ("Player");
-		homingObj = GameObject.Find ("Enemy");
+	void Start()
+	{
+		rigidbody2D = GetComponent<Rigidbody2D>();
+		lifeScript = GameObject.FindGameObjectWithTag ("HP").GetComponent<Life> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		this.transform.position = Vector2.MoveTowards (this.transform.position, new Vector2 (Player.transform.position.x, Player.transform.position.y), Speed * Time.deltaTime);
+
+	void Update()
+	{
+		//ghost
+		TimeCount -= Time.deltaTime;
+		if (_isRendered) {
+			rigidbody2D.velocity = new Vector2 (speed, rigidbody2D.velocity.y);
+		}
+		if (TimeCount <= 0) {
+			//画像をx軸のみに対して反転
+			transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+			if (ghost_m == 1) {
+				speed = -2;
+				ghost_m = 0;
+			} else {
+				speed = 2;
+				ghost_m = 1;
+			}
+			TimeCount = 4;
+		}
+		//gameover
+		if (gameObject.transform.position.y < Camera.main.transform.position.y - 8
+			|| gameObject.transform.position.x < Camera.main.transform.position.x - 10) {
+			Destroy (gameObject);
+		}
+	}
+
+	//爆発処理2
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (_isRendered) {
+			if (col.tag == "Bullet") {
+				Destroy (gameObject);
+				Instantiate (explosion, transform.position, transform.rotation);
+				if (Random.Range (0, 2) == 0) {
+					Instantiate (item, transform.position, transform.rotation);
+				}
+			}
+		}
+	}
+
+	//HP
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		//unitychanとぶつかった時
+		if (col.gameObject.tag == "UnityChan") {
+			//LifeScriptのLifeDownメソッドを実行
+			lifeScript.LifeDown(attackPoint);
+		}
+	}
+
+	//待機
+	void OnWillRenderObject() {
+		//メインカメラに映った時だけ_isRenderedをtrue
+		if (Camera.current.tag == MAIN_CAMERA_TAG_NAME) {
+			_isRendered = true;
+		}
 	}
 }
---
+++ Shinobu_Mono/Assets/Scripts/PlayerTriggerObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTriggerObject : MonoBehaviour
+{
+	private string playerTag = "UnityChan";
+	private bool isOn = false;
+	private bool callFixed = false;
+
+	/// <summary>
+	/// プレイヤーが上に乗っているかどうか
+	/// </summary>
+	/// <returns><c>true</c>, if player on was ised, <c>false</c> otherwise.</returns>
+	public bool IsPlayerOn()
+	{
+		return isOn;
+	}
+
+	private void FixedUpdate()
+	{
+		callFixed = true;
+	}
+
+	private void LateUpdate()
+	{
+		if (callFixed)
+		{
+			//フラグを元に戻します
+			isOn = false;
+			callFixed = false;
+		}
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.tag == playerTag)
+		{
+			//プレイヤーの下から４分の１が範囲内にいる時乗っているとみなします
+			if(collision.transform.position.y - (collision.bounds.size.y / 4.0f) > transform.position.y)
+			{
+				isOn = true;
+			}
+		}
+	}
+}
